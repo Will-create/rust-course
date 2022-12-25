7d85951d4a67b9a0fab1a2ecbdb81b76090ac3ff
Louis Bertson <louisbertson@gmail.com>
2022-12-25 12:17:25 +0000

    Added client app

--- chat/client/src/main.rs
+++ chat/client/src/main.rs
@@ -18,8 +18,40 @@ fn main() {
         client.read_exact(&mut buff) {
             Ok(_) => {
                 let msg = buff.into_iter().take_while(|&x| x! = 0).collect::<Vec<_>>();
+                println!("Message recv");
+        
+            },
+            Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+            Err(_) => {
+                println!("Connection with server severed");
+                break;
             }
         }
-    })
+
+        match rx.try_recv() {
+            Ok(msg) => {
+                let mut buff = msg.clone().into_bytes();
+                buff.resize(MSG_SIZE, 0);
+                client.write_all(&buff).expect("Writing to the socket failed");
+                println!("Message sent: {:?}", msg);
+            },
+            Err(TryRecvError::Empty) => (),
+            Err(TryRecvError::Disconnected) => break;
+
+        }
+
+        thread::sleep(Duration::from_millis(100));
+    });
+
+
+    println!("Please write your message");
+    loop {
+        let mut buff = String::new();
+        io::stdin().read_line(&mut buff).expect("Reading from stdin failed");
+        let msg = buff.trim().to_string();
+        if msg == ":q" || tx.send(msg).is_err() {break}
+    }
+
+    println!("Bye Bye!");
 
 }
